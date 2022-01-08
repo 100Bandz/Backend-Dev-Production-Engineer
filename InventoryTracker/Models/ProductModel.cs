0ad935f93e7e169d5edfd2a23bed21f79d41c675
100Bandz <nbl0357@gmail.com>
2022-01-07 19:43:20 -0500

    Create ProductModel.cs
    Added the appropriate properties for any product

---
+++ InventoryTracker/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryTracker.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public ProductModel()
+        {
+
+        }
+    }
+}

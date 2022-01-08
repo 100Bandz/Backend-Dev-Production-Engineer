b7c64dd965db8534dd130c622c494039049df2be
100Bandz <nbl0357@gmail.com>
2022-01-07 19:46:25 -0500

    Create InventoryController.cs
    tests the ProcessAdd action  and the Index action

---
+++ InventoryTracker/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using InventoryTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryTracker.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessAdd(ProductModel productModel)
+        {
+            if (productModel.Name == "Nico" && productModel.Quantity == 25)
+            {
+                return View("AddSuccess", productModel);
+            }
+            else
+            {
+                return View("AddFailure", productModel);
+            }
+
+        }
+
+    }
+}

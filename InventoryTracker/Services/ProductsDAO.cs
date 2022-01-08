cf8c3c47baf2e5567b232d2b983fcd47f184f2fc
100Bandz <nbl0357@gmail.com>
2022-01-07 20:35:53 -0500

    Create ProductsDAO.cs
    DAO SQL DB server connection

---
+++ InventoryTracker/Services/ProductsDAO.cs
@@ -0,0 +1,25 @@
+﻿using InventoryTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryTracker.Services
+{
+    public class ProductsDAO
+    {
+        string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Inventory;
+             Integrated Security=True;" + "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
+            "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+        public bool FindProductByNameAndPrice(ProductModel product)
+        {
+            string SQLQuery = "SELECT * FROM dbo.Products WHERE name = @name AND price = @price");
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+        }
+
+    }
+}

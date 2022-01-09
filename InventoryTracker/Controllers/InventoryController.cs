479b7c7c71bca6072994cb2bcfaf9c0cb3aa443b
100Bandz <nbl0357@gmail.com>
2022-01-08 22:51:37 -0500

    Update InventoryController.cs
    Created the IActionResult methods for each user interaction

--- InventoryTracker/Controllers/InventoryController.cs
+++ InventoryTracker/Controllers/InventoryController.cs
@@ -10,7 +10,14 @@ namespace InventoryTracker.Controllers
 {
     public class InventoryController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index()    //List of Inventory
+        {
+            ProductsDAO products = new ProductsDAO();
+
+            return View(products.GetAllProducts());
+        }
+
+        public IActionResult Add()
         {
             return View();
         }
@@ -18,9 +25,11 @@ namespace InventoryTracker.Controllers
         public IActionResult ProcessAdd(ProductModel productModel)
         {
             SecurityService securityService = new SecurityService();
+            ProductsDAO products = new ProductsDAO();
 
             if (securityService.IsValidProduct(productModel))
             {
+                products.Add(productModel);
                 return View("AddSuccess", productModel);
             }
             else
@@ -30,5 +39,46 @@ namespace InventoryTracker.Controllers
 
         }
 
+        public IActionResult SearchAR(string searchTerm)    
+        {
+            ProductsDAO products = new ProductsDAO();
+            List<ProductModel> productList = products.SearchProducts(searchTerm);
+
+            return View("Index", productList);
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        public IActionResult ShowDetails(int id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel foundProduct = products.GetProductById(id);
+            return View("Details",foundProduct);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel foundProduct = products.GetProductById(id);
+            return View("Edit",foundProduct);
+        }
+
+        public IActionResult ProcessEdit(ProductModel productModel)
+        {
+            ProductsDAO products = new ProductsDAO();
+            products.Update(productModel);
+            return View("Index", products.GetAllProducts());
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            ProductsDAO products = new ProductsDAO();
+            ProductModel productModel = products.GetProductById(Id);
+            products.Delete(productModel);
+            return View("Index", products.GetAllProducts());
+        }
     }
 }

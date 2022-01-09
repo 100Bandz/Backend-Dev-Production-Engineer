818e2eeb96c9c6c54a71448989721abc810d6fc9
100Bandz <nbl0357@gmail.com>
2022-01-08 22:45:37 -0500

    Update ProductsDAO.cs
    The Data Access Object Pattern used for all the logical operations in the inventory management system

--- InventoryTracker/Services/ProductsDAO.cs
+++ InventoryTracker/Services/ProductsDAO.cs
@@ -7,19 +7,193 @@ using System.Threading.Tasks;
 
 namespace InventoryTracker.Services
 {
-    public class ProductsDAO
+    public class ProductsDAO : IProductDataService 
     {
-        string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Inventory;
-             Integrated Security=True;" + "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
-            "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Inventory;Integrated Security=True;
+        Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-
-        public bool FindProductByNameAndPrice(ProductModel product)
+        public int Delete(ProductModel product)
         {
-            string SQLQuery = "SELECT * FROM dbo.Products WHERE name = @name AND price = @price");
+            int newIdNumber = -1;
 
-            SqlConnection connection = new SqlConnection(ConnectionString);
+            string SQLQuery = "DELETE FROM dbo.Product WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+                command.Parameters.AddWithValue("@Id", product.Id);
+                command.Parameters.AddWithValue("@Name", product.Name);
+                command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@Description", product.Description);
+                command.Parameters.AddWithValue("@Date", product.Date);
+
+                try
+                {
+                    connection.Open();
+                    newIdNumber = Convert.ToInt32(command.ExecuteScalar());
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return newIdNumber;
         }
 
+        public List<ProductModel> GetAllProducts()
+        {
+            List<ProductModel> foundProducts = new List<ProductModel>();
+
+            string SQLQuery = "SELECT * FROM dbo.Product";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        foundProducts.Add(new ProductModel { Id = (int)reader[0], Name = (string)reader[1], Quantity = (int)reader[2], Price = (decimal)reader[3], Description = (string)reader[4], Date = (DateTime)reader[5] });
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return foundProducts;
+        }
+
+        public ProductModel GetProductById(int id)
+        {
+            ProductModel foundProduct = null;
+
+            string SQLQuery = "SELECT * FROM dbo.Product WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        foundProduct = new ProductModel { Id = (int)reader[0], Name = (string)reader[1], Quantity = (int)reader[2], 
+                                      Price = (decimal)reader[3], Description = (string)reader[4], Date = (DateTime)reader[5] };
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return foundProduct;
+        }
+
+        public int Add(ProductModel product)
+        {
+
+            string SQLQuery = "INSERT INTO dbo.Product(Name, Quantity, Price, Description, Date) VALUES (@Name, @Quantity, @Price, @Description, @Date)";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+
+                command.Parameters.AddWithValue("@Name", product.Name);
+                command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@Description", product.Description);
+                command.Parameters.AddWithValue("@Date", product.Date);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return 1;
+        }
+
+        public List<ProductModel> SearchProducts(string searchTerm)
+        {
+            List<ProductModel> foundProducts = new List<ProductModel>();
+
+            string SQLQuery = "SELECT * FROM dbo.Product WHERE Name LIKE @Name";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+                command.Parameters.AddWithValue("@Name",'%' + searchTerm + '%');
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        foundProducts.Add(new ProductModel { Id = (int)reader[0], Name = (string)reader[1], Quantity = (int)reader[2], Price = (decimal)reader[3], Description = (string)reader[4], Date = (DateTime)reader[5] });
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return foundProducts;
+        }
+
+        public int Update(ProductModel product)
+        {
+            int newIdNumber = -1;
+
+            string SQLQuery = "UPDATE dbo.Product SET Name = @Name, Quantity = @Quantity, Price = @Price, Description = @Description, Date = @Date WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(SQLQuery, connection);
+                command.Parameters.AddWithValue("@Id", product.Id);
+                command.Parameters.AddWithValue("@Name", product.Name);
+                command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                command.Parameters.AddWithValue("@Description", product.Description);
+                command.Parameters.AddWithValue("@Date", product.Date);
+
+                try
+                {
+                    connection.Open();
+                    newIdNumber = Convert.ToInt32(command.ExecuteScalar());
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return newIdNumber;
+        }
     }
 }

93227aabbaa36a748ad034ace14a9aa1e7cdeedb
100Bandz <nbl0357@gmail.com>
2022-01-08 22:47:16 -0500

    Update IProductDataService.cs
    A simple Interface that defines the contract for any class that implements it

--- InventoryTracker/Services/IProductDataService.cs
+++ InventoryTracker/Services/IProductDataService.cs
@@ -11,7 +11,7 @@ namespace InventoryTracker.Services
         List<ProductModel> GetAllProducts();
         List<ProductModel> SearchProducts(string searchTerm);
         ProductModel GetProductById(int id);
-        int Insert(ProductModel product);
+        int Add(ProductModel product);
         int Delete(ProductModel product);
         int Update(ProductModel product);
     }

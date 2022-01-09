d39656aee6833e37011cfb96e9dad5681794a97f
100Bandz <nbl0357@gmail.com>
2022-01-08 22:43:51 -0500

    Create SecurityService.cs
    Currently only checks to make sure an item that is going to be added, is not already in the database

---
+++ InventoryTracker/Services/SecurityService.cs
@@ -0,0 +1,39 @@
+﻿using InventoryTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryTracker.Services
+{
+    public class SecurityService
+    {
+        ProductsDAO productDAO = new ProductsDAO();
+
+        public SecurityService()
+        {
+
+        }
+
+        public bool IsValidProduct(ProductModel product)    //Checks if an item by the same name is already in the database
+        {
+            //bool isNullOrEmpty = productDAO.SearchProducts(product.Name)?.Any() != true;
+            //return isNullOrEmpty;
+
+            if (productDAO.SearchProducts(product.Name)?.Any() != true)
+            {
+                return true;
+            }
+
+            foreach (ProductModel item in productDAO.SearchProducts(product.Name)) // Loop through List with foreach
+            {
+                if (item.Name == product.Name)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}

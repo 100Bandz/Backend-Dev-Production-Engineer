4b679de71e2d21aad84efe8d8815f857424c2fdb
100Bandz <nbl0357@gmail.com>
2022-01-14 21:58:35 -0500

    Update SecurityService.cs
    Removed old code

--- InventoryTracker/Services/SecurityService.cs
+++ InventoryTracker/Services/SecurityService.cs
@@ -17,23 +17,6 @@ namespace InventoryTracker.Services
 
         public bool IsValidProduct(ProductModel product)    //Checks if an item by the same name is already in the database
         {
-            //bool isNullOrEmpty = productDAO.SearchProducts(product.Name)?.Any() != true;
-            //return isNullOrEmpty;
-
-            //if (productDAO.SearchProducts(product.Name)?.Any() != true)
-            //{
-            //    return true;
-            //}
-
-            //foreach (ProductModel item in productDAO.SearchProducts(product.Name)) // Loop through List with foreach
-            //{
-            //    if (item.Name == product.Name)
-            //    {
-            //        return false;
-            //    }
-            //}
-            //return true;
-
             if (productDAO.SearchName(product.Name))
             {
                 return true;

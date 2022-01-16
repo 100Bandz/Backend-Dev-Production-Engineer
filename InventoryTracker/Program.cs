481dfc2d42b56db048623755c52cb6ef672b8356
100Bandz <nbl0357@gmail.com>
2022-01-16 18:31:58 -0500

    backup stuff

--- InventoryTracker/Program.cs
+++ InventoryTracker/Program.cs
@@ -1,9 +1,11 @@
+using InventoryTracker.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +15,8 @@ namespace InventoryTracker
     {
         public static void Main(string[] args)
         {
+
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,6 +24,13 @@ namespace InventoryTracker
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    //var root = Directory.GetCurrentDirectory();
+                    //var dotenv = Path.Combine(root, ".env");
+                    //DotEnv.Load(dotenv);
+
+                    //var config = new ConfigurationBuilder()
+                    //    .AddEnvironmentVariables()
+                    //    .Build();
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- InventoryTracker/Startup.cs
+++ InventoryTracker/Startup.cs
@@ -23,6 +23,8 @@ namespace InventoryTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //var SQLCONSTR = Environment.GetEnvironmentVariable("SQLCS");
+
             services.AddControllersWithViews();
         }
 
